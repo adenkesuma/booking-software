cd91c100d460293410bbeb0a46d7c883055b5d7d
adenkesuma <adenfdfd10@gmail.com>
2023-05-31 21:04:02 +0700

    create Calendar and dateTime for booking

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,62 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import ReactCalendar from 'react-calendar'
+import { add, format } from 'date-fns'
+import { INTERVAL, STORE_CLOSING_TIME, STORE_OPENING_TIME } from '../constants/config'
 
 interface CalendarProps {}
 
+interface DateType {
+  justDate: Date | null
+  dateTime: Date | null
+}
+
 const Calendar: FC<CalendarProps> = () => {
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  })
+  
+  console.log(date.dateTime)
+  console.log(date.justDate)
+
+  const getTimes = () => {
+    if (!date.justDate) return
+    const { justDate } = date
+
+    const beginning = add(justDate, { hours: STORE_OPENING_TIME })
+    const end = add(justDate, { hours: STORE_CLOSING_TIME })
+    const interval = INTERVAL
+
+    const times = []
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i)
+    }
+    return times
+  }
+
+  const times = getTimes()
+
   return (
-    <ReactCalendar 
-      minDate={new Date()}
-      className='REACT-CALENDAR p-2'
-      view='month'
-      onClickDay={(date) => console.log(date)}
-    />
+    <div className='flex h-screen flex-col items-center justify-center'>
+     {date.justDate ? (
+      <div className="flex gap-4">
+        {times?.map((time, i) => (
+          <div key={`time-${i}`} className='rounded-sm bg-gray-100 p-2'>
+            <button type='button' onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}>
+              {format(time, 'kk:mm')}
+            </button>
+          </div>
+        ))}
+      </div>
+     ) : (
+      <ReactCalendar 
+        minDate={new Date()}
+        className='REACT-CALENDAR p-2'
+        view='month'
+        onClickDay={(date) => setDate((prev) => ({...prev, justDate: date}))}
+      />
+     )}
+    </div>
   )
 }
 
---
+++ src/constants/config.ts
@@ -0,0 +1,3 @@
+export const STORE_OPENING_TIME = 9 // in hours
+export const STORE_CLOSING_TIME = 17 // in hours
+export const INTERVAL = 30 // in minutes  
